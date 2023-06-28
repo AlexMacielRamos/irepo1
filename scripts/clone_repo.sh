0d5e8a3c2bf03d8746ab37ff72e5684315d80712
Alex Maciel <69065904+AlexMacielRamos@users.noreply.github.com>
2023-06-28 16:52:55 -0600

    Update clone_repo.sh

--- scripts/clone_repo.sh
+++ scripts/clone_repo.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-echo 'hola' > aaaaaaaaaaafile.txt
 cd usr/share
 git clone https://github.com/AlexMacielRamos/scriptTest.git
 echo 'se clono repositorio con exito'
